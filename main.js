df8a877f734aabbae83bcb2616172481a12f9adb
USERCODEHOST <108839721+USERCODEHOST@users.noreply.github.com>
2023-07-06 09:09:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+myCanvas = document.getElementById("myCanvas");
+ canvas = document.getElementById("myCanvas");
+ ctx = canvas.getContext("2d");
+{
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(20, 100);
+ctx.lineTo(70, 100);
+ctx.stroke();}
